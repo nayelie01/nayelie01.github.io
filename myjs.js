f7cb295e9cfe783c37aef346eca11d046be4a645
Nayelie Napoles <nayelie.napoles@gmail.com>
2019-07-19 15:23:06 -0700

    took out map

--- myjs.js
+++ myjs.js
@@ -1,37 +1,37 @@
 alert("Welcome to my website");
 
-var view;
-var map;
-var initialLocation;
-var london;
-
-function init() {
-  initialLocation = ol.proj.fromLonLat([41.043316, 28.862457]);
-  london = ol.proj.fromLonLat([-0.12755, 51.507222]);
-
-  view = new ol.View({
-    center: initialLocation,
-    zoom: 6
-  });
-
-
-  map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    loadTilesWhileAnimating: true,
-    view: view
-  });
-}
-
-function panLondon() {
-  view.animate({
-    center: london,
-    duration: 2000
-  })
-}
-
-window.onload = init;
+// var view;
+// var map;
+// var initialLocation;
+// var london;
+//
+// function init() {
+//   initialLocation = ol.proj.fromLonLat([41.043316, 28.862457]);
+//   london = ol.proj.fromLonLat([-0.12755, 51.507222]);
+//
+//   view = new ol.View({
+//     center: initialLocation,
+//     zoom: 6
+//   });
+//
+//
+//   map = new ol.Map({
+//     target: 'map',
+//     layers: [
+//       new ol.layer.Tile({
+//         source: new ol.source.OSM()
+//       })
+//     ],
+//     loadTilesWhileAnimating: true,
+//     view: view
+//   });
+// }
+//
+// function panLondon() {
+//   view.animate({
+//     center: london,
+//     duration: 2000
+//   })
+// }
+//
+// window.onload = init;
